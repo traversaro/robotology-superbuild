8c9ff6c1fd1950f0ad2511b1f0b7a5df2f63f134
Silvio Traversaro <silvio.traversaro@iit.it>
2021-02-10 09:38:44 +0100

    Update bld.bat

--- conda/recipe_template/bld.bat
+++ conda/recipe_template/bld.bat
@@ -2,14 +2,13 @@ mkdir build
 cd build
 
 :: Hardcoding Visual Studio 2019 as GitHub Actions does not have VS2019
+:: -DBUILD_SHARED_LIBS=ON for now disabled as a workaround for https://github.com/robotology/icub-main/issues/717 
 cmake ^
     -G"Visual Studio 16 2019" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
-{# Build shared libs for now disabled as a workaround for https://github.com/robotology/icub-main/issues/717 #}
-{# -DBUILD_SHARED_LIBS=ON ^ #}
 {% for cmake_arg in cmake_args %}    {{ cmake_arg }} ^
 {% endfor %}    %SRC_DIR%
 if errorlevel 1 exit 1
